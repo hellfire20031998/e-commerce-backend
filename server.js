23960ca2195f2abc00c7e44fc48deccd64150c3c
hellfire20031998 <tonu8707@gmail.com>
2024-04-06 13:47:04 +0530

    My first

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port= 3002;
-const productRoute=require("./routes_product");
+const productRoute=require("./src/product/routes_product");
 // const router = require("../Student/routes");
 
 
